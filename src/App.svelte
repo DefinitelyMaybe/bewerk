9a4b83c3da30c3f5da4f1c1012c8883b2d7b77af
Rawr <DefinitelyMaybe@users.noreply.github.com>
2020-05-11 22:29:58 +1200

    removed slotted node

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,13 @@
 	import Editor from './editor.svelte';
 	
 	let name = 'world';
+
+	// const node = new HTMLElement('Node')
+	// document.getElementById('app').appendChild(node)
 </script>
 
+<style>
+</style>
+
 <Editor>
-	<Node></Node>
 </Editor>
