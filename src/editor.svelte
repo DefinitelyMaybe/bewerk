d70d6638a83c69d1022fd581976119e3e1c73fb5
Rawr <DefinitelyMaybe@users.noreply.github.com>
2020-05-18 20:52:39 +1200

    scrolling over graph is a thing

--- src/editor.svelte
+++ src/editor.svelte
@@ -2,16 +2,21 @@
 	import Node from './node.svelte'
 	
 	let nodes = [
-		{ x:250, y:250, component:Node },
+		{ x:200, y:200, component:Node },
 		{ x:250, y:300, component:Node },
-		{ x:250, y:350, component:Node }
+		{ x:300, y:400, component:Node }
 	]
 	let currentNode = undefined
 
 	let dragXOffSet = 0
 	let dragYOffSet = 0
-	let backgroundXOffSet = 0
-	let backgroundYOffSet = 0
+
+	// let initDragMovement = true
+	// let prevX = 0
+	// let prevY = 0
+
+	let backgroundOffsetX = 0
+	let backgroundOffsetY = 0
 
 	// note: node positions may not work if the editor is not placed
 	// in the top left corner (for now)
@@ -21,6 +26,10 @@
 	// create an origin and position nodes relative to the origin
 
 	// pick up on the drag events for panning across the graph
+	
+	// context menu for right click event
+	// on import cursor change to waiting?
+
 	function handleDragStart(event) {
 		// check what was dragged
 		if (!currentNode) {
@@ -52,11 +61,22 @@
 		}
 	}
 
-	// context menu for right click event
-	// on import cursor change to waiting?
-
-	function handleDragOver (event) {
+	function handleDragOver(event) {
 		event.preventDefault()
+		// Later on, you might like to revisit this code so that
+		// you can drag instead of scrolling.
+		// if (!currentNode) {
+		// 	// console.log(event)
+		// 	if (!initDragMovement) {
+		// 		event.target.scroll(event.screenX - prevX, event.screenY - prevY)
+		// 		prevX = event.screenX
+		// 		prevY = event.screenY
+		// 	} else {
+		// 		prevX = event.screenX
+		// 		prevY = event.screenY
+		// 		initDragMovement = false
+		// 	}
+		// }
 	}
 
 	function handleDragEnd(event) {
@@ -64,8 +84,8 @@
 		// this should only happen if the node was dropped in the editor
 		if (currentNode) {
 			// update both the node style and the editor array
-			let y = event.clientY - dragYOffSet
-			let x = event.clientX - dragXOffSet
+			let y = event.clientY - dragYOffSet + backgroundOffsetY
+			let x = event.clientX - dragXOffSet + backgroundOffsetX
 
 			// assume that the id and position match within the arSay = 0
 			let backgroundYOffSet = 0
@@ -79,10 +99,14 @@
 		}
 	}
 	
-	function handleDrop (event) {
+	function handleDrop(event) {
 		event.preventDefault()
 	}
 
+	function handleScroll(event) {
+		backgroundOffsetX = event.target.scrollLeft
+		backgroundOffsetY = event.target.scrollTop
+	}
 </script>
 
 <style>
@@ -95,25 +119,27 @@
 		width: 800px;
 		height: 600px;
 		clip-path: inset(0px 0px 0px 0px);
-		background-color: #fff;
-  	background-size: 40px 40px;
-  	background-image:
-    	linear-gradient(to right, #9ae5ff 1px, transparent 1px),
-    	linear-gradient(to bottom, #9ae5ff 1px, transparent 1px);
-		overflow: hidden;
+		overflow: scroll;
 		border: solid black 1px;
 	}
 
 	.background {
 		width: 3508px;
 		height: 2480px;
+		background-color: #fff;
+  	background-size: 40px 40px;
+  	background-image:
+    	linear-gradient(to right, #9ae5ff 1px, transparent 1px),
+    	linear-gradient(to bottom, #9ae5ff 1px, transparent 1px);
 	}
 </style>
 
+{backgroundOffsetX}, {backgroundOffsetY}
 <main on:dragover={handleDragOver}
 	on:dragend={handleDragEnd}
 	on:drop={handleDrop}
 	on:dragstart={handleDragStart}
+	on:scroll={handleScroll}
 	on:mousedown={handleMouseDown}
 	draggable="true">
 	<div class="background">
