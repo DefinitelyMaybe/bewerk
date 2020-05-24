750ad82be21d57d4458f419bda41da7030cf9c10
Rawr <DefinitelyMaybe@users.noreply.github.com>
2020-05-24 20:06:13 +1200

    positioning and possible menu items added

--- src/ui/menu.svelte
+++ src/ui/menu.svelte
@@ -1,17 +1,24 @@
 <script>
-  export let open = false
+  export let visible = false
 </script>
 
 <style>
   ul {
     position: absolute;
+    margin: 0px;
+    z-index: 1;
     top: 0px;
     left: 0px;
+    background-color: white;
+    border: solid 2px;
   }
 </style>
 
-<ul>
-  <li>one</li>
-  <li>two</li>
-  <li>three</li>
+<ul id="menu"
+  style="visibility:{visible?'visible':'hidden'};">
+  <li>new</li>
+  <li>edit</li>
+  <li>delete</li>
+  <li>load</li>
+  <li>save</li>
 </ul>
